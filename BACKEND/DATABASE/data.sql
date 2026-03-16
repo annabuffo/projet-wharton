6ce6a5b2a05769cfb05321bc53e40d352c422011
annabuffo <anna.buffo@oclock.school>
2026-03-16 09:45:02 +0100

    ajout bdd sql

---
+++ BACKEND/DATABASE/data.sql
@@ -0,0 +1,44 @@
+-----------------------------
+--TABLE :  public.admin
+-----------------------------
+
+CREATE TABLE public."admin" (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT VARCHAR(255) NOT NULL,
+    password TEXT VARCHAR(50) NOT NULL,
+    pseudo TEXT VARCHAR(100) NOT NULL,
+    role ADMIN,
+    date_inscripton TIMESTAMP 
+)
+
+-----------------------------
+--TABLE :  public.admin
+-----------------------------
+
+CREATE TABLE public."discussions" (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    
+)
+
+-----------------------------
+--TABLE :  public.admin
+-----------------------------
+
+
+
+-----------------------------
+--TABLE :  public.admin
+-----------------------------
+
+
+
+-----------------------------
+--TABLE :  public.admin
+-----------------------------
+
+
+
+-----------------------------
+--TABLE :  public.admin
+-----------------------------
+25
